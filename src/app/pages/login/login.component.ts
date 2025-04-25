ae80818d9dca7e6416f89edc0391133e3f940d2b
Wesley Sanches Bahia <sistema@bruzonmussi.com.br>
2025-04-24 21:57:10 -0300

    add

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+enum  Mode {
+  DARK = "dark",
+  LIGHT = "light"
+}
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit   {
 
+  constructor(){}
+  public mode:Mode = Mode.DARK;
+  ngOnInit(): void {
+    document.body.classList.toggle(this.mode);
+  }
+
+
+  toggleTheme() {
+    if(this.mode == "dark"){
+      document.body.classList.toggle(this.mode);
+      this.mode = Mode.LIGHT;
+    }else {
+      this.mode = Mode.DARK;
+      document.body.classList.toggle(this.mode);
+    }
+  }
 }
