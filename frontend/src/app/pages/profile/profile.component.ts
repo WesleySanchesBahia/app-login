ad10f9ae3d7fc3f9c25acb61aaa1f18f7b252771
Wesley Sanches Bahia <sistema@bruzonmussi.com.br>
2025-04-29 16:10:04 -0300

    card profile criado

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
 import { ThemeModeComponent } from "../../components/theme-mode/theme-mode.component";
 import { LoggedUserService } from '../../services/logged-user.service';
 import { User } from '../../../types/User';
+import VanillaTilt from 'vanilla-tilt';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +11,31 @@ import { User } from '../../../types/User';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewChecked {
 
-  constructor(private user: LoggedUserService){
+  constructor(private user: LoggedUserService, private router:Router){
 
   }
   profile!:User | null;
   ngOnInit(): void {
   this.profile = this.user.data;
   }
+
+
+  ngAfterViewChecked(): void {
+    let element = document.querySelector(".card") as HTMLElement;
+
+    VanillaTilt.init(element, {
+      max:25,
+      speed:400,
+      glare:true,
+      "max-glare":0.4
+    })
+  }
+
+
+  logout(): void{
+    this.router.navigate(["/"]);
+  }
+
 }
