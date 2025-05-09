78948cb028a19a48fc8872d4ccb903babf3eb073
Wesley Sanches Bahia <sistema@bruzonmussi.com.br>
2025-05-09 09:23:35 -0300

    url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ import { Enviromment } from "./environment.interface";
 
 export const environment:Enviromment = {
     currentEnvironment: "development",
-    apiUrl:"http://localhost:3000/api/"
+    apiUrl:"https://app-login-cnqp.onrender.com/api/"
 };
